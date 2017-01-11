3035a3c0aa65a527ae4767e1c5b39eec540c270c
Charles Peden <ccbpeden@warpmail.net>
2017-01-11 09:42:47 -0800

    replace mad libs with form letter, transform input to uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var person1Input = $("input#person1").val();
-
-    $(".person1").append(person1Input);
+    var person1uppercase = person1Input.toUpperCase()
+    $(".person1").append(person1uppercase);
 
     $("#story").show();
 
